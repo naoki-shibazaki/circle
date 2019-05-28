9318af48f374c4e22bba067bd2986593ff576d48
tomo <suzawa@photomo.jp>
2019-05-28 12:53:46 +0900

    学生団体追加

--- db/fixtures/event.rb
+++ db/fixtures/event.rb
@@ -2,6 +2,7 @@ Event.seed(:id,
 
   { :id => 0, :name => "選択してください" , :ruby => "nil", :icon => "nil" },
   { :id => 1, :name => "写真・カメラ" , :ruby => "camera", :icon => "fa fa-camera-retro" },
-  { :id => 2, :name => "バスケットボール" , :ruby => "basketball", :icon => "fa fa-group" }
+  { :id => 2, :name => "バスケットボール" , :ruby => "basketball", :icon => "fa fa-group" },
+  { :id => 3, :name => "学生団体" , :ruby => "student", :icon => "fa fa-flag" }
 
 )
