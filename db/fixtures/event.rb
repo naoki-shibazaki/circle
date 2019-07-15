8b70579a46fafb49b0989a9f578883433ef5fcf0
tomo <suzawa@photomo.jp>
2019-07-15 15:38:00 +0900

    並び替え　スポーツ

--- db/fixtures/event.rb
+++ db/fixtures/event.rb
@@ -4,10 +4,10 @@ Event.seed(:id,
   { :id => 1, :order => 7, :name => "写真・カメラ" , :ruby => "camera", :icon => "fa fa-camera-retro" },
   { :id => 2, :order => 1, :name => "バスケットボール" , :ruby => "basketball", :icon => "fa fa-dribbble" },
   { :id => 3, :order => 8, :name => "ランニング" , :ruby => "running", :icon => "fa fa-child" },
-  { :id => 4, :order => 2, :name => "バレーボール" , :ruby => "volleyball", :icon => "fa fa-group" },
-  { :id => 5, :order => 4, :name => "バドミントン" , :ruby => "badminton", :icon => "fa fa-group" },
+  { :id => 4, :order => 6, :name => "バレーボール" , :ruby => "volleyball", :icon => "fa fa-group" },
+  { :id => 5, :order => 2, :name => "バドミントン" , :ruby => "badminton", :icon => "fa fa-group" },
   { :id => 6, :order => 9, :name => "筋トレ" , :ruby => "work-out", :icon => "fa fa-trophy" },
   { :id => 7, :order => 5, :name => "野球・ソフトボール" , :ruby => "baseball", :icon => "fa fa-dribbble" },
   { :id => 8, :order => 3, :name => "フットサル" , :ruby => "futsal", :icon => "fa fa-soccer-ball-o" },
-  { :id => 9, :order => 6, :name => "テニス" , :ruby => "tennis", :icon => "fa fa-group" }
+  { :id => 9, :order => 4, :name => "テニス" , :ruby => "tennis", :icon => "fa fa-group" }
 )
