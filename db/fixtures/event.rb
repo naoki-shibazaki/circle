e7a40c9eecff1634e6cab164eb704750c2bf69b4
tomo <suzawa@photomo.jp>
2019-07-18 00:48:44 +0900

    種目追加

--- db/fixtures/event.rb
+++ db/fixtures/event.rb
@@ -1,13 +1,16 @@
 Event.seed(:id,
 
   { :id => 0, :order => 0, :name => "選択してください" , :ruby => "nil", :icon => "nil" },
-  { :id => 1, :order => 7, :name => "写真・カメラ" , :ruby => "camera", :icon => "fa fa-camera-retro" },
-  { :id => 2, :order => 1, :name => "バスケットボール" , :ruby => "basketball", :icon => "fa fa-dribbble" },
-  { :id => 3, :order => 8, :name => "ランニング" , :ruby => "running", :icon => "fa fa-child" },
-  { :id => 4, :order => 6, :name => "バレーボール" , :ruby => "volleyball", :icon => "fa fa-group" },
-  { :id => 5, :order => 2, :name => "バドミントン" , :ruby => "badminton", :icon => "fa fa-group" },
-  { :id => 6, :order => 9, :name => "筋トレ" , :ruby => "work-out", :icon => "fa fa-trophy" },
-  { :id => 7, :order => 5, :name => "野球・ソフトボール" , :ruby => "baseball", :icon => "fa fa-dribbble" },
-  { :id => 8, :order => 3, :name => "フットサル" , :ruby => "futsal", :icon => "fa fa-soccer-ball-o" },
-  { :id => 9, :order => 4, :name => "テニス" , :ruby => "tennis", :icon => "fa fa-group" }
+  { :id => 1, :order => "g", :name => "写真・カメラ" , :ruby => "camera", :icon => "fa fa-camera-retro" },
+  { :id => 2, :order => "a", :name => "バスケットボール" , :ruby => "basketball", :icon => "fa fa-dribbble" },
+  { :id => 3, :order => "h", :name => "ランニング" , :ruby => "running", :icon => "fa fa-child" },
+  { :id => 4, :order => "f", :name => "バレーボール" , :ruby => "volleyball", :icon => "fa fa-group" },
+  { :id => 5, :order => "b", :name => "バドミントン" , :ruby => "badminton", :icon => "fa fa-group" },
+  { :id => 6, :order => "k", :name => "筋トレ" , :ruby => "work-out", :icon => "fa fa-trophy" },
+  { :id => 7, :order => "e", :name => "野球・ソフトボール" , :ruby => "baseball", :icon => "fa fa-dribbble" },
+  { :id => 8, :order => "c", :name => "フットサル" , :ruby => "futsal", :icon => "fa fa-soccer-ball-o" },
+  { :id => 9, :order => "d", :name => "テニス" , :ruby => "tennis", :icon => "fa fa-group" },
+  { :id => 10, :order => "j", :name => "卓球" , :ruby => "table-tennis", :icon => "fa fa-group" },
+  { :id => 11, :order => "i", :name => "ゲーム" , :ruby => "game", :icon => "fa fa-gamepad" },
+  { :id => 12, :order => "l", :name => "イベント" , :ruby => "event", :icon => "fa fa-flag" }
 )
