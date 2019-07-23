b8ad8d1c6683840880f17de6c012aa0c99cf13a1
tomo <suzawa@photomo.jp>
2019-07-24 01:46:10 +0900

    エラーページ

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -46,7 +46,7 @@ class BlogsController < ApplicationController
 		# パンくず
 		@b1_name = "活動ブログ"
 		@b1_url = "/blogs"
-		@b2_name = "#{@blog.title.truncate(18)}"
+		@b2_name = "#{@blog.title.truncate(16)}"
 		@b2_url = ""
 	end
 
