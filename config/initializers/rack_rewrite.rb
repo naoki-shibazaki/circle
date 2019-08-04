06da3f31fd75e1cf6e0c5ef6444ab3069c80d7af
tomo <suzawa@photomo.jp>
2019-08-05 00:14:53 +0900

    ファイル削除

--- config/initializers/rack_rewrite.rb
+++
@@ -1,7 +0,0 @@
-if ENV['RACK_ENV'] == 'production'
-  MyAppName::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    r301 %r{.*}, 'https://www.circle-book.com$&', :if => Proc.new {|rack_env|
-      rack_env['SERVER_NAME'] == 'circle-book.com'
-    }
-  end
-end
