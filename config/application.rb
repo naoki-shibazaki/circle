a18fd9cc061fa7965497e484e8dbdae7d5d46020
tomo <suzawa@photomo.jp>
2019-08-06 11:19:28 +0900

    301

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'rack/rewrite'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,5 +14,14 @@ module Circle
     config.time_zone = 'Tokyo'
 
 	config.i18n.default_locale = :ja
+  end
+end
+
+
+if ENV['RACK_ENV'] == 'production'
+  config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+    r301 %r{.*}, 'https://www.circle-book.com$&', :if => Proc.new { |rack_env|
+      rack_env['SERVER_NAME'] != 'circle-book.com'
+    }
   end
 end
