0761f850adf7bc5878c52ccffabde091093aaf8f
tomo <suzawa@photomo.jp>
2019-08-06 10:50:56 +0900

    301リダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,24 @@ class ApplicationController < ActionController::Base
 	before_action :request_path
 	before_action :set_data
 
+	protect_from_forgery with: :exception
+
+	  #herokuapp.comから独自ドメインへリダイレクト
+	  before_filter :ensure_domain
+	  FQDN = 'www.circle-book.com'
+	 
+	  # redirect correct server from herokuapp domain for SEO
+	  def ensure_domain
+	   return unless /\.herokuapp.com/ =~ request.host
+	   
+	   # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める 
+	   port = ":#{request.port}" unless [80, 443].include?(request.port)
+	   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+	  end
+
+	  
+
+
 	def set_current_user
     	@current_user = User.find_by(id: session[:user_id])
   	end
