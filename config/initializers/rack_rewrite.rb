c54deb85d1ac4da0acf045d43df5c12eb51dc415
tomo <suzawa@photomo.jp>
2019-08-06 13:18:30 +0900

    301

--- config/initializers/rack_rewrite.rb
+++
@@ -1,7 +0,0 @@
-if ENV['RACK_ENV'] == 'production'
-  config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    r301 %r{.*}, 'https://www.circle-book.com$&', :if => Proc.new {|rack_env|
-      rack_env['SERVER_NAME'] == 'circle-book.herokuapp.com'
-    }
-  end
-end
