950af9f340e11eb6285bfdb79d3fc4cbed7b5da7
tomo <suzawa@photomo.jp>
2019-08-09 10:37:13 +0900

    ページネーション

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -2,9 +2,9 @@
 Kaminari.configure do |config|
   # config.default_per_page = 10
   # config.max_per_page = nil
-  config.window = 2
+  # config.window = 2
   # config.outer_window = 0
-  config.left = 8
+  config.left = 7
   config.right = 1
   # config.page_method_name = :page
   # config.param_name = :page
