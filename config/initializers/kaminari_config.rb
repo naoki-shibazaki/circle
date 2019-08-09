40bca191946db4d94888ddf480a27c19a1542812
tomo <suzawa@photomo.jp>
2019-08-09 10:29:16 +0900

    ページネーション

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -4,8 +4,8 @@ Kaminari.configure do |config|
   # config.max_per_page = nil
   config.window = 2
   # config.outer_window = 0
-  config.left = 7
-  config.right = 0
+  config.left = 8
+  config.right = 1
   # config.page_method_name = :page
   # config.param_name = :page
   # config.params_on_first_page = false
