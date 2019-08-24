67f673fd8c5ab36ee58a9a35865660cc3b3ac096
tomo <suzawa@photomo.jp>
2019-08-24 13:31:40 +0900

    CloudFront

--- config/application.rb
+++ config/application.rb
@@ -14,15 +14,3 @@ module Circle
 	config.i18n.default_locale = :ja
   end
 end
-
-
-
-# config.middleware.insert_before 0, "Rack::Cors" do
-#   allow do
-#     origins '*'
-#     resource '*',
-#       :headers => 'Content-*',
-#       :methods => [:get],
-#       :max_age => 0
-#   end
-# end
