82bbcb6f76374f5b3d3b5f72cc37ebecab016c5e
tomo <suzawa@photomo.jp>
2019-09-01 15:15:50 +0900

    sitemap 優先度変更

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -16,7 +16,7 @@ SitemapGenerator::Sitemap.create do
   end
 
   Blog.find_each do |blog|
-    add blog_path(blog), :lastmod => blog.updated_at, :priority => 0.7, :changefreq => 'daily'
+    add blog_path(blog), :lastmod => blog.updated_at, :priority => 0.5, :changefreq => 'daily'
   end
 
 
@@ -25,14 +25,14 @@ SitemapGenerator::Sitemap.create do
   Prefecture.find_each do |prefecture|
 
     if prefecture.kana != "nil"
-      add "/prefectures/#{prefecture.kana}", :lastmod => prefecture.updated_at, :priority => 0.5, :changefreq => 'daily'
-      add "/blog/prefectures/#{prefecture.kana}", :lastmod => prefecture.updated_at, :priority => 0.5, :changefreq => 'daily'
+      add "/prefectures/#{prefecture.kana}", :lastmod => prefecture.updated_at, :priority => 0.8, :changefreq => 'daily'
+      add "/blog/prefectures/#{prefecture.kana}", :lastmod => prefecture.updated_at, :priority => 0.3, :changefreq => 'weekly'
 
           Group.find_each do |prefecture_group|
-                add "/prefectures/#{prefecture.kana}/#{prefecture_group.group}", :lastmod => prefecture_group.updated_at, :priority => 0.5, :changefreq => 'daily'
+                add "/prefectures/#{prefecture.kana}/#{prefecture_group.group}", :lastmod => prefecture_group.updated_at, :priority => 0.3, :changefreq => 'weekly'
 
                 Age.find_each do |prefecture_age|
-                      add "/prefectures/#{prefecture.kana}/#{prefecture_group.group}/#{prefecture_age.decade}", :lastmod => prefecture_age.updated_at, :priority => 0.5, :changefreq => 'daily'
+                      add "/prefectures/#{prefecture.kana}/#{prefecture_group.group}/#{prefecture_age.decade}", :lastmod => prefecture_age.updated_at, :priority => 0.3, :changefreq => 'weekly'
                 end
 
           end
@@ -47,21 +47,21 @@ SitemapGenerator::Sitemap.create do
 
     if event.ruby != "nil"
 
-      add "/#{event.ruby}", :lastmod => event.updated_at, :priority => 0.5, :changefreq => 'daily'
-      add "/blog/#{event.ruby}", :lastmod => event.updated_at, :priority => 0.5, :changefreq => 'daily'
+      add "/#{event.ruby}", :lastmod => event.updated_at, :priority => 0.8, :changefreq => 'daily'
+      add "/blog/#{event.ruby}", :lastmod => event.updated_at, :priority => 0.3, :changefreq => 'weekly'
 
         Prefecture.find_each do |event_prefecture|
           
                 if event_prefecture.kana != "nil"
-                  add "/#{event.ruby}/#{event_prefecture.kana}", :lastmod => event_prefecture.updated_at, :priority => 0.5, :changefreq => 'daily'
-                  add "/blog/#{event.ruby}/#{event_prefecture.kana}", :lastmod => event_prefecture.updated_at, :priority => 0.5, :changefreq => 'daily'
+                  add "/#{event.ruby}/#{event_prefecture.kana}", :lastmod => event_prefecture.updated_at, :priority => 1.0, :changefreq => 'daily'
+                  add "/blog/#{event.ruby}/#{event_prefecture.kana}", :lastmod => event_prefecture.updated_at, :priority => 0.3, :changefreq => 'weekly'
                
                         Age.find_each do |age|
-                              add "/#{event.ruby}/#{event_prefecture.kana}/#{age.decade}", :lastmod => age.updated_at, :priority => 0.5, :changefreq => 'daily'
+                              add "/#{event.ruby}/#{event_prefecture.kana}/#{age.decade}", :lastmod => age.updated_at, :priority => 0.3, :changefreq => 'weekly'
                         end
 
                         Group.find_each do |group|
-                              add "/#{event.ruby}/#{event_prefecture.kana}/#{group.group}", :lastmod => group.updated_at, :priority => 0.5, :changefreq => 'daily'
+                              add "/#{event.ruby}/#{event_prefecture.kana}/#{group.group}", :lastmod => group.updated_at, :priority => 0.3, :changefreq => 'weekly'
                         end
 
                  end
