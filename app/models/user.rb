de266c51bdde33308ff4f431ad57ef9c11488e40
tomo <suzawa@photomo.jp>
2019-10-01 19:50:58 +0900

    タグ色変更 表示件数15件

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
 	  	:format => URI::regexp(%w(http https)),
 	}
 
-	paginates_per 10
+	paginates_per 15
 	
 	before_save do
 		self.average_age.gsub!(/[\[\]\"]/, "") if attribute_present?("average_age")
