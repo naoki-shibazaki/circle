8af362d027e4c105fcdd2a5f41acefe5f17926c0
tomo <suzawa@photomo.jp>
2019-10-02 21:49:02 +0900

    カテゴリ修正

--- db/fixtures/event.rb
+++ db/fixtures/event.rb
@@ -13,17 +13,20 @@ Event.seed(:id,
   { :id => 3, :category => "スポーツ２", :order => "22", :name => "ランニング" , :ruby => "running", :icon => "fa fa-child" },
   { :id => 10, :category => "スポーツ２", :order => "23", :name => "卓球" , :ruby => "table-tennis", :icon => "fa fa-group" },
   { :id => 14, :category => "スポーツ２", :order => "24", :name => "ウィッフルボール" , :ruby => "wiffleball", :icon => "fa fa-dribbble" },
-  { :id => 13, :category => "スポーツ２", :order => "25", :name => "ゴルフ" , :ruby => "golf", :icon => "fa fa-map-pin" },
-  { :id => 17, :category => "スポーツ２", :order => "26", :name => "ロードバイク" , :ruby => "road-bike", :icon => "fa fa-bicycle" },
+  { :id => 17, :category => "スポーツ２", :order => "25", :name => "ロードバイク" , :ruby => "road-bike", :icon => "fa fa-bicycle" },
+  { :id => 15, :category => "スポーツ２", :order => "26", :name => "オールラウンド" , :ruby => "all-round", :icon => "fa fa-group" },
 
   { :id => 1, :category => "その他", :order => "51", :name => "写真・カメラ" , :ruby => "camera", :icon => "fa fa-camera-retro" },
-  { :id => 15, :category => "その他", :order => "52", :name => "オールラウンド" , :ruby => "all-round", :icon => "fa fa-group" },
-  { :id => 12, :category => "その他", :order => "53", :name => "イベント" , :ruby => "event", :icon => "fa fa-flag" },
+  { :id => 12, :category => "その他", :order => "52", :name => "イベント" , :ruby => "event", :icon => "fa fa-flag" },
+  { :id => 21, :category => "その他", :order => "53", :name => "ボルダリング" , :ruby => "bouldering", :icon => "fa fa-group" },
   { :id => 11, :category => "その他", :order => "54", :name => "ゲーム" , :ruby => "game", :icon => "fa fa-gamepad" },
   { :id => 6, :category => "その他", :order => "55", :name => "筋トレ・合トレ" , :ruby => "work-out", :icon => "fa fa-trophy" },
   { :id => 18, :category => "その他", :order => "56", :name => "登山" , :ruby => "mountain", :icon => "fa fa-photo" },
   { :id => 19, :category => "その他", :order => "57", :name => "ダンス" , :ruby => "dance", :icon => "fa fa-group" },
   { :id => 20, :category => "その他", :order => "58", :name => "スノーボード" , :ruby => "snow-board", :icon => "fa fa-group" },
-  { :id => 21, :category => "その他", :order => "59", :name => "ボルダリング" , :ruby => "bouldering", :icon => "fa fa-group" }
+  { :id => 13, :category => "その他", :order => "59", :name => "音楽" , :ruby => "golf", :icon => "fa fa-music" }
 
 )
+
+
+
