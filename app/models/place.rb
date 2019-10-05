2a0e5e44ea3f53d965ec7b902b825e0aded535af
tomo <suzawa@photomo.jp>
2019-10-05 15:45:19 +0900

    place 修正

--- app/models/place.rb
+++ app/models/place.rb
@@ -9,5 +9,6 @@ class Place < ApplicationRecord
   	validates :event_id, presence: true
   	validates :name, presence: true
   	validates :tag, presence: true
+  	validates :address, presence: true
 
 end
