ba43b138b5ff3cca907760e20097dc15838f0266
tomo <suzawa@photomo.jp>
2020-03-18 22:24:01 +0900

    kaminari

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,12 +1,13 @@
-# frozen_string_literal: true
 Kaminari.configure do |config|
-  # config.default_per_page = 10
-  # config.max_per_page = nil
-  # config.window = 2
-  # config.outer_window = 0
-  config.left = 7
-  config.right = 1
-  # config.page_method_name = :page
-  # config.param_name = :page
+
+  # config.default_per_page = 10 # 1ページ辺りの項目数
+  # config.max_per_page = nil    # 1ページ辺りの最大数
+  config.window = 8            # ex 値が2の場合 .. 2 3 (4) 5 6 ..
+  # config.outer_window = 0      # ex 値が2の場合 .. (4) .. 99 100
+  # config.left = 7              # ...になったときの左側の表示数
+  # config.right = 1             # ...になったときの右側の表示数
+  # config.page_method_name = :page # メソッド名
+  # config.param_name = :page    # ページネーションのパラメーターの名前
   # config.params_on_first_page = false
+
 end
