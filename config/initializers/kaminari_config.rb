1fdbf32c083027fe7e476db668816d59362defd7
tomo <suzawa@photomo.jp>
2020-03-18 22:43:36 +0900

    kaminari

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -2,7 +2,7 @@ Kaminari.configure do |config|
 
   # config.default_per_page = 10 # 1ページ辺りの項目数
   # config.max_per_page = nil    # 1ページ辺りの最大数
-  config.window = 4            # ex 値が2の場合 .. 2 3 (4) 5 6 ..
+  config.window = 8            # ex 値が2の場合 .. 2 3 (4) 5 6 ..
   # config.outer_window = 0      # ex 値が2の場合 .. (4) .. 99 100
   # config.left = 7              # ...になったときの左側の表示数
   # config.right = 1             # ...になったときの右側の表示数
