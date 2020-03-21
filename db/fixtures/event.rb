f6e4e831affc4dfcebf2613b2d5c3c73ebbe86cb
tomo <suzawa@photomo.jp>
2020-03-21 21:15:46 +0900

    種目追加

--- db/fixtures/event.rb
+++ db/fixtures/event.rb
@@ -3,26 +3,41 @@ Event.seed(:id,
   { :id => 2, :category => "スポーツ１", :order => "01", :name => "バスケ" , :ruby => "basketball",  :openchat => "https://line.me/ti/g2/28860kzjimldNe2P9MOsqg", :icon => "fa fa-dribbble" },
   { :id => 4, :category => "スポーツ１", :order => "02", :name => "バレーボール" , :ruby => "volleyball",  :openchat => "https://line.me/ti/g2/VRCpbJsgXIinQcwSOyw0uQ", :icon => "fa fa-group" },
   { :id => 8, :category => "スポーツ１", :order => "03", :name => "フットサル" , :ruby => "futsal",  :openchat => "https://line.me/ti/g2/jq0AbQjRV3XsyDnmvNidtA", :icon => "fa fa-soccer-ball-o" },
-  { :id => 9, :category => "スポーツ１", :order => "04", :name => "テニス" , :ruby => "tennis",  :openchat => "https://line.me/ti/g2/rQkRsdC0Ppg-xVTQVlQYgw", :icon => "fa fa-group" },
-  { :id => 7, :category => "スポーツ１", :order => "05", :name => "野球" , :ruby => "baseball",  :openchat => "https://line.me/ti/g2/StLPRlAGJPjl9rHX9BTXug", :icon => "fa fa-dribbble" },
-  { :id => 16, :category => "スポーツ１", :order => "06", :name => "サッカー" , :ruby => "soccer",  :openchat => "https://line.me/ti/g2/TuGt6TZqgewnQONxcJd7Yw", :icon => "fa fa-soccer-ball-o" },
 
-  { :id => 5, :category => "スポーツ２", :order => "21", :name => "バドミントン" , :ruby => "badminton",  :openchat => "https://line.me/ti/g2/9hLTdMzYeuiuO1fRinT_wQ", :icon => "fa fa-group" },
-  { :id => 3, :category => "スポーツ２", :order => "22", :name => "ランニング" , :ruby => "running",  :openchat => "", :icon => "fa fa-child" },
-  { :id => 10, :category => "スポーツ２", :order => "23", :name => "卓球" , :ruby => "table-tennis",  :openchat => "", :icon => "fa fa-group" },
+  { :id => 5, :category => "スポーツ１", :order => "04", :name => "バドミントン" , :ruby => "badminton",  :openchat => "https://line.me/ti/g2/9hLTdMzYeuiuO1fRinT_wQ", :icon => "fa fa-group" },
+  { :id => 9, :category => "スポーツ１", :order => "05", :name => "硬式・軟式テニス" , :ruby => "tennis",  :openchat => "https://line.me/ti/g2/rQkRsdC0Ppg-xVTQVlQYgw", :icon => "fa fa-group" },
+  { :id => 10, :category => "スポーツ１", :order => "06", :name => "卓球" , :ruby => "table-tennis",  :openchat => "", :icon => "fa fa-group" }, 
+ 
+  { :id => 7, :category => "スポーツ１", :order => "07", :name => "草野球" , :ruby => "baseball",  :openchat => "https://line.me/ti/g2/StLPRlAGJPjl9rHX9BTXug", :icon => "fa fa-dribbble" },
+  { :id => 22, :category => "スポーツ１", :order => "08", :name => "ソフトボール" , :ruby => "softball",  :openchat => "https://line.me/ti/g2/StLPRlAGJPjl9rHX9BTXug", :icon => "fa fa-group" },
+  { :id => 16, :category => "スポーツ１", :order => "09", :name => "サッカー" , :ruby => "soccer",  :openchat => "https://line.me/ti/g2/TuGt6TZqgewnQONxcJd7Yw", :icon => "fa fa-soccer-ball-o" },
+
+
+# --------------------------------------
+
+  { :id => 3, :category => "スポーツ２", :order => "21", :name => "ランニング" , :ruby => "running",  :openchat => "", :icon => "fa fa-child" },
+  { :id => 21, :category => "スポーツ２", :order => "22", :name => "ボルダリング" , :ruby => "bouldering",  :openchat => "", :icon => "fa fa-group" },
+  { :id => 23, :category => "スポーツ２", :order => "23", :name => "ビーチバレー" , :ruby => "beach-volleyball",  :openchat => "", :icon => "fa fa-group" },
+
   { :id => 17, :category => "スポーツ２", :order => "24", :name => "ロードバイク" , :ruby => "road-bike",  :openchat => "", :icon => "fa fa-bicycle" },
-  { :id => 21, :category => "スポーツ２", :order => "25", :name => "ボルダリング" , :ruby => "bouldering",  :openchat => "", :icon => "fa fa-group" },
+  { :id => 6, :category => "スポーツ２", :order => "25", :name => "筋トレ・合トレ" , :ruby => "work-out",  :openchat => "", :icon => "fa fa-trophy" },
   { :id => 15, :category => "スポーツ２", :order => "26", :name => "オールラウンド" , :ruby => "all-round",  :openchat => "", :icon => "fa fa-group" },
 
+
+# --------------------------------------
+
   { :id => 1, :category => "その他", :order => "51", :name => "写真・カメラ" , :ruby => "camera",  :openchat => "https://line.me/ti/g2/MrGVZOgwkBEGzRpvagL-Cw", :icon => "fa fa-camera-retro" },
-  { :id => 12, :category => "その他", :order => "52", :name => "イベント" , :ruby => "event",  :openchat => "", :icon => "fa fa-flag" },
-  { :id => 11, :category => "その他", :order => "53", :name => "ゲーム" , :ruby => "game",  :openchat => "", :icon => "fa fa-gamepad" },
-  { :id => 6, :category => "その他", :order => "54", :name => "筋トレ・合トレ" , :ruby => "work-out",  :openchat => "", :icon => "fa fa-trophy" },
-  { :id => 13, :category => "その他", :order => "55", :name => "音楽" , :ruby => "music",  :openchat => "", :icon => "fa fa-music" },
-  { :id => 18, :category => "その他", :order => "56", :name => "登山" , :ruby => "mountain",  :openchat => "", :icon => "fa fa-photo" },
-  { :id => 19, :category => "その他", :order => "57", :name => "ダンス" , :ruby => "dance",  :openchat => "", :icon => "fa fa-group" },
-  { :id => 14, :category => "その他", :order => "58", :name => "よさこい" , :ruby => "yosakoi",  :openchat => "", :icon => "fa fa-dribbble" },
-  { :id => 20, :category => "その他", :order => "59", :name => "スノーボード" , :ruby => "snow-board",  :openchat => "", :icon => "fa fa-group" }
+  { :id => 14, :category => "その他", :order => "52", :name => "よさこい" , :ruby => "yosakoi",  :openchat => "", :icon => "fa fa-dribbble" },
+  { :id => 19, :category => "その他", :order => "53", :name => "ダンス" , :ruby => "dance",  :openchat => "", :icon => "fa fa-group" },
+
+  { :id => 12, :category => "その他", :order => "54", :name => "イベント" , :ruby => "event",  :openchat => "", :icon => "fa fa-flag" },
+  { :id => 11, :category => "その他", :order => "55", :name => "ゲーム" , :ruby => "game",  :openchat => "", :icon => "fa fa-gamepad" },
+  { :id => 13, :category => "その他", :order => "56", :name => "音楽" , :ruby => "music",  :openchat => "", :icon => "fa fa-music" },
+
+  { :id => 18, :category => "その他", :order => "57", :name => "登山" , :ruby => "mountain",  :openchat => "", :icon => "fa fa-photo" },
+  { :id => 20, :category => "その他", :order => "58", :name => "スノーボード" , :ruby => "snow-board",  :openchat => "", :icon => "fa fa-group" },
+  { :id => 24, :category => "その他", :order => "59", :name => "学生団体" , :ruby => "student-group",  :openchat => "", :icon => "fa fa-group" }
+
 
 )
 
