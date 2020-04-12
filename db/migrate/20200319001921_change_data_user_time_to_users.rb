8fd76f40fa6a96869db67ece8459d961381f6982
tomo <suzawa@photomo.jp>
2020-04-12 12:23:20 +0900

    マイグレーション変更

--- db/migrate/20200319001921_change_data_user_time_to_users.rb
+++ db/migrate/20200319001921_change_data_user_time_to_users.rb
@@ -1,5 +1,5 @@
 class ChangeDataUserTimeToUsers < ActiveRecord::Migration[5.2]
   def change
-  	change_column :users, :user_time, :date
+  	change_column :users, :user_time, 'date USING CAST(user_time AS date)'
   end
 end
