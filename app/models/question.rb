53b068e78f3cbabb8d52e223251a5da4c39683df
tomo <suzawa@photomo.jp>
2020-04-13 22:40:27 +0900

    Question 編集

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true, length: { in: 1..100 }
+  validates :content, presence: true, length: { in: 1..80 }
 end
