c0dd43b2032233d67e05fbba2e46d8604b8daa8f
tomo <suzawa@photomo.jp>
2020-05-17 01:35:16 +0900

    エラー migrate　ファイル　ロールバック

--- db/migrate/20200516100041_add_member_to_member.rb
+++ db/migrate/20200516100041_add_member_to_member.rb
@@ -1,6 +1,6 @@
 class AddMemberToMember < ActiveRecord::Migration[5.2]
   def change
-  	add_reference :members, :event_question, index: true
+  	# add_reference :members, :event_question, index: true
   	add_column :members, :nickname, :string
     add_column :members, :image_profile, :string
     add_column :members, :answer, :text
