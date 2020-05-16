e08eed708b8cd7440605461b2e9d39f2eaac542b
tomo <suzawa@photomo.jp>
2020-05-16 10:36:25 +0900

    top文言変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ def default_meta_tags
       reverse: true,
       charset: 'utf-8',
       description: '【登録不要・無料で使える！】全国のスポーツ・趣味のサークルメンバー募集サイト！社会人や学生、初心者、経験者など、誰でも参加可能！バスケやバレー、フットサルなど無料で参加、メンバーの募集が簡単にできます！',
-      keywords: 'サークル,スポーツ,趣味,社会人,学生',
+      keywords: 'サークル,スポーツ,趣味,団体,社会人,学生',
       canonical: request.original_url,
       separator: '|',
       icon: [
