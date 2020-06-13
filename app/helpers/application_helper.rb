60cb29244f79a99ea1e503d385016db9bf1b48c3
tomo <suzawa@photomo.jp>
2020-06-13 13:51:23 +0900

    画像遅延読み込み

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,32 +1,43 @@
 module ApplicationHelper
 
-def default_meta_tags
-    {
-      site: 'サークルブック',
-      title: 'サークルブック',
-      reverse: true,
-      charset: 'utf-8',
-      description: '【登録不要・無料で使える！】47都道府県のサークルやチーム、団体メンバーの募集サイト！スポーツや趣味など豊富に掲載！社会人や学生、初心者、経験者など、誰でも参加可能！バスケやバレー、フットサルなど無料でメンバーの募集ができます！',
-      keywords: 'サークル,チーム,団体,スポーツ,趣味,社会人,学生',
-      canonical: request.original_url,
-      separator: '|',
-      icon: [
-        { href: image_url('/images/icon.jpg') },
-        { href: image_url('/images/icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
-      ],
-      og: {
-        site_name: :site,
-        title: :title,
-        description: :description,
-        type: 'website',
-        url: request.original_url,
-        image: image_url('/images/ogp.png'),
-        locale: 'ja_JP'
-      },
-      twitter: {
-          card: 'summary_large_image'
-      }      
-    }
- end
+  def default_meta_tags
+      {
+        site: 'サークルブック',
+        title: 'サークルブック',
+        reverse: true,
+        charset: 'utf-8',
+        description: '【登録不要・無料で使える！】47都道府県のサークルやチーム、団体メンバーの募集サイト！スポーツや趣味など豊富に掲載！社会人や学生、初心者、経験者など、誰でも参加可能！バスケやバレー、フットサルなど無料でメンバーの募集ができます！',
+        keywords: 'サークル,チーム,団体,スポーツ,趣味,社会人,学生',
+        canonical: request.original_url,
+        separator: '|',
+        icon: [
+          { href: image_url('/images/icon.jpg') },
+          { href: image_url('/images/icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+        ],
+        og: {
+          site_name: :site,
+          title: :title,
+          description: :description,
+          type: 'website',
+          url: request.original_url,
+          image: image_url('/images/ogp.png'),
+          locale: 'ja_JP'
+        },
+        twitter: {
+            card: 'summary_large_image'
+        }      
+      }
+  end
+
+    def lazysizes_image_tag(source, options={})
+      options['data-src'] = source
+      if options[:class].blank?
+        options[:class] = "lazyload"
+      else
+        options[:class] = "lazyload #{options[:class]}"
+      end
+       image_tag("/images/loading.gif", options) + ("<noscript>#{image_tag(source, options)}</noscript>").html_safe
+    end
+
 
 end
