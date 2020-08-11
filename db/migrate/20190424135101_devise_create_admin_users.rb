a7faaeea5858fffe394471316c151070e665a7a2
須沢友裕 <suzawa@suzawaiMac.local>
2020-08-11 23:10:33 +0900

    パスワード再発行　実装

--- db/migrate/20190424135101_devise_create_admin_users.rb
+++ db/migrate/20190424135101_devise_create_admin_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DeviseCreateAdminUsers < ActiveRecord::Migration[5.2]
-  def change
+def change
     create_table :admin_users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
