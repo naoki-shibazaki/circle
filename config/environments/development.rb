eac023cfcbffff92f939c6315454fe1607d9a2f0
須沢友裕 <suzawa@suzawaiMac.local>
2020-08-11 22:43:26 +0900

    パスワード再発行　実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['MAIL_USER_NAME'],
+    password:             ENV['MAIL_PASSWORD'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }  
+
+
 end
