20367674969de8477a3824338dff8dba23972789
須沢友裕 <suzawa@suzawaiMac.local>
2020-09-22 14:02:35 +0900

    レビュー機能

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :member, optional: true
+
+
+	with_options presence: true do
+	  validates :comment
+	end
+
+
 end
