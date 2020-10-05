e2945d688262f47839167136435bac35e0868755
tomo <suzawa@photomo.jp>
2020-10-05 10:58:54 +0900

    sub Place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -174,12 +174,9 @@ class PlacesController < ApplicationController
 		@events = Event.all
 		@event = Event.find_by(ruby: params[:ruby])		
 		@event_places = @event.places_events.map{|e| e.place.id}
-		# @event_ids = PlacesEvent.where(event_id: @event.id)
-		# @event_places = @event_ids.map { |e| e.place_id }
-		@prefectures = Prefecture.where.not(id: 50).order(:order => :asc)
-
-		# @cities = City.all.order(:id => :asc)
-
+		@event_ids = PlacesEvent.where(event_id: @event.id)
+		@event_places = @event_ids.map { |e| e.place_id }
+		# @prefectures = Prefecture.where.not(id: 50).order(:order => :asc)
 
     	@b1_name = "コート情報"
 		@b1_url = "/places"
