a28664b37014aaf007d816ac17857fa75bd88440
須沢友裕 <suzawa@suzawaiMac.local>
2020-10-08 17:12:45 +0900

    Schedule show デザイン変更

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -96,6 +96,7 @@ before_action :set_schedules
 		def set_schedules
 			@user = User.find(params[:user_id])		
 			@schedules = Schedule.where(user_id: @user.id).where("day > ?", DateTime.yesterday).order(:day => :asc)
+			@past_schedules = Schedule.where(user_id: @user.id).where("day < ?", DateTime.yesterday).order(:day => :desc)
 			@data = AdminUser.find_by(id: params[:user_id])
 			@prefectures = Prefecture.all
 			@schedule_month = 0
