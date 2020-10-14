8df911f10214db6a6028416a55e30160b58b2a2a
須沢友裕 <suzawa@suzawaiMac.local>
2020-10-14 23:41:42 +0900

    ブックマーク機能 AJAX対応

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,27 +2,26 @@ class BookmarksController < ApplicationController
 
   # before_action :authenticate_member!
 	before_action :ensure_correct_member
+  before_action :set_user, {only: [:create, :destroy]}
 
   def create
-    @user = User.find(params[:user_id])
-    bookmark = @user.bookmarks.new(member_id: current_member.id)
-    if bookmark.save
-    	flash[:notice] = "ブックマーク登録しました！"
-      	redirect_to request.referer
-    else
-      redirect_to request.referer
+    @bookmark = @user.bookmarks.new(member_id: current_member.id)
+    if @bookmark.save
+    	# flash[:notice] = "ブックマーク登録しました！"
+      	# redirect_to request.referer
+    # else
+      # redirect_to request.referer
     end
   end
 
   def destroy
-    @user = User.find(params[:user_id])
-    bookmark = @user.bookmarks.find_by(member_id: current_member.id)
-    if bookmark.present?
-        bookmark.destroy
-        flash[:notice] = "ブックマーク解除しました！"
-        redirect_to request.referer
-    else
-        redirect_to request.referer
+    @bookmark = @user.bookmarks.find_by(member_id: current_member.id)
+    if @bookmark.present?
+        @bookmark.destroy
+        # flash[:notice] = "ブックマーク解除しました！"
+        # redirect_to request.referer
+    # else
+        # redirect_to request.referer
     end
   end
 
@@ -38,6 +37,10 @@ private
 
 		end
 	end  
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 
 
 
