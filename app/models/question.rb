abd32dd908b0d5d4dc152b4127e2de6bb85157cc
須沢友裕 <suzawa@suzawaiMac.local>
2021-01-01 16:48:14 +0900

    Question NGワード追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { in: 1..80 }
 
-	NGWORD = %w(http 死ね)
+	NGWORD = %w(http 死ね 京築リーグ)
 	NGWORD_REGEX = %r(#{NGWORD.join('|')})
 	validates :content, format: { without: NGWORD_REGEX }
 
