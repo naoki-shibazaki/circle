32025229e859de6c8970b9b0a9c59d37b4611a4a
須沢友裕 <suzawa@suzawaiMac.local>
2021-02-09 11:03:04 +0900

    管理画面 ブックマーク数　反映

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,12 @@
 class BookmarksController < ApplicationController
 
-  # before_action :authenticate_member!
 	before_action :ensure_correct_member
   before_action :set_user, {only: [:create, :destroy]}
 
   def create
     @bookmark = @user.bookmarks.new(member_id: current_member.id)
     if @bookmark.save
-    	# flash[:notice] = "ブックマーク登録しました！"
-      	# redirect_to request.referer
-    # else
-      # redirect_to request.referer
+
     end
   end
 
@@ -18,25 +14,17 @@ class BookmarksController < ApplicationController
     @bookmark = @user.bookmarks.find_by(member_id: current_member.id)
     if @bookmark.present?
         @bookmark.destroy
-        # flash[:notice] = "ブックマーク解除しました！"
-        # redirect_to request.referer
-    # else
-        # redirect_to request.referer
+
     end
   end
 
-
 private
 	def ensure_correct_member
-
 		if member_signed_in?
-
 		else
-
-	      redirect_to new_member_registration_path
-
+      redirect_to new_member_registration_path
 		end
-	end  
+	end
 
   def set_user
     @user = User.find(params[:user_id])
