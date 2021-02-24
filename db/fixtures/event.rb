73fe17072ffa6c57adfbc78865dd3bafcbaff724
須沢友裕 <suzawa@suzawaiMac.local>
2021-02-24 12:04:19 +0900

    event 追加

--- db/fixtures/event.rb
+++ db/fixtures/event.rb
@@ -3,44 +3,73 @@ Event.seed(:id,
 # matching => Match 1:連携/2:複数連携/0:非掲載
 # place => Place 1:連携/0:非連携
 
-  { :id => 2, :category_id => 1, :order => "01", :name => "バスケ" , :ruby => "basketball", :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/28860kzjimldNe2P9MOsqg", :txt => "バスケサークル・チーム" },
-  { :id => 4, :category_id => 1, :order => "02", :name => "バレーボール" , :ruby => "volleyball",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/J2oirsmlRsHc1QVS4sFb6Q", :txt => "バレーサークル・チーム" },
-  { :id => 8, :category_id => 1, :order => "03", :name => "フットサル" , :ruby => "futsal",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/jq0AbQjRV3XsyDnmvNidtA", :txt => "フットサルサークル・チーム" },
-  { :id => 7, :category_id => 1, :order => "07", :name => "草野球" , :ruby => "baseball",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/StLPRlAGJPjl9rHX9BTXug", :txt => "草野球チーム・サークル" },
-  { :id => 22, :category_id => 1, :order => "08", :name => "ソフトボール" , :ruby => "softball",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/StLPRlAGJPjl9rHX9BTXug", :txt => "ソフトボールチーム・サークル" },
-  { :id => 16, :category_id => 1, :order => "09", :name => "サッカー" , :ruby => "soccer",  :matching => 1, :place => 1, :openchat => "", :txt => "サッカーチーム・サークル" },
-  { :id => 25, :category_id => 1, :order => "27", :name => "ハンドボール" , :ruby => "handball",  :matching => 1, :place => 1, :openchat => "", :txt => "ハンドボールサークル・チーム" },
+  # 球技スポーツ
+  { :id => 2, :category_id => 1, :order => "0101", :name => "バスケ" , :ruby => "basketball", :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/28860kzjimldNe2P9MOsqg", :txt => "バスケサークル・チーム" },
+  { :id => 4, :category_id => 1, :order => "0102", :name => "バレーボール" , :ruby => "volleyball",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/J2oirsmlRsHc1QVS4sFb6Q", :txt => "バレーサークル・チーム" },
+  { :id => 8, :category_id => 1, :order => "0103", :name => "フットサル" , :ruby => "futsal",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/jq0AbQjRV3XsyDnmvNidtA", :txt => "フットサルサークル・チーム" },
+  { :id => 7, :category_id => 1, :order => "0104", :name => "草野球" , :ruby => "baseball",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/StLPRlAGJPjl9rHX9BTXug", :txt => "草野球チーム・サークル" },
+  { :id => 22, :category_id => 1, :order => "0105", :name => "ソフトボール" , :ruby => "softball",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/StLPRlAGJPjl9rHX9BTXug", :txt => "ソフトボールチーム・サークル" },
+  { :id => 16, :category_id => 1, :order => "0106", :name => "サッカー" , :ruby => "soccer",  :matching => 1, :place => 1, :openchat => "", :txt => "サッカーチーム・サークル" },
+  { :id => 25, :category_id => 1, :order => "0107", :name => "ハンドボール" , :ruby => "handball",  :matching => 1, :place => 1, :openchat => "", :txt => "ハンドボールサークル・チーム" },
+  { :id => 23, :category_id => 1, :order => "0108", :name => "ドッジボール" , :ruby => "dodgeball",  :matching => 0, :place => 0, :openchat => "", :txt => "ドッジボールサークル" },
 
-  { :id => 5, :category_id => 2, :order => "04", :name => "バドミントン" , :ruby => "badminton",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/9hLTdMzYeuiuO1fRinT_wQ", :txt => "バドミントンサークル・クラブ" },
-  { :id => 9, :category_id => 2, :order => "05", :name => "テニス" , :ruby => "tennis",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/rQkRsdC0Ppg-xVTQVlQYgw", :txt => "テニスサークル" },
-  { :id => 10, :category_id => 2, :order => "06", :name => "卓球" , :ruby => "table-tennis",  :matching => 1, :place => 1, :openchat => "", :txt => "卓球サークル・クラブ" },
+  # ラケットスポーツ
+  { :id => 5, :category_id => 2, :order => "0201", :name => "バドミントン" , :ruby => "badminton",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/9hLTdMzYeuiuO1fRinT_wQ", :txt => "バドミントンサークル・クラブ" },
+  { :id => 9, :category_id => 2, :order => "0202", :name => "テニス" , :ruby => "tennis",  :matching => 1, :place => 1, :openchat => "https://line.me/ti/g2/rQkRsdC0Ppg-xVTQVlQYgw", :txt => "テニスサークル" },
+  { :id => 10, :category_id => 2, :order => "0203", :name => "卓球" , :ruby => "table-tennis",  :matching => 1, :place => 1, :openchat => "", :txt => "卓球サークル・クラブ" },
 
-  { :id => 3, :category_id => 3, :order => "21", :name => "ランニング" , :ruby => "running",  :matching => 0, :place => 0, :openchat => "", :txt => "ランニング・マラソンサークル" },
-  { :id => 21, :category_id => 3, :order => "22", :name => "ボルダリング" , :ruby => "bouldering",  :matching => 0, :place => 0, :openchat => "", :txt => "ボルダリングサークル" },
-  { :id => 15, :category_id => 3, :order => "56", :name => "オールラウンド" , :ruby => "all-round",  :matching => 2, :place => 0, :openchat => "", :txt => "オールラウンドサークル" },
-  { :id => 23, :category_id => 3, :order => "29", :name => "ビーチスポーツ" , :ruby => "beach-sports",  :matching => 1, :place => 0, :openchat => "", :txt => "ビーチスポーツ" },
+  # その他のスポーツ
+  { :id => 3, :category_id => 3, :order => "0301", :name => "ランニング" , :ruby => "running",  :matching => 0, :place => 0, :openchat => "", :txt => "ランニング・マラソンサークル" },
+  { :id => 21, :category_id => 3, :order => "0302", :name => "ボルダリング" , :ruby => "bouldering",  :matching => 0, :place => 0, :openchat => "", :txt => "ボルダリングサークル" },
+  { :id => 28, :category_id => 3, :order => "0303", :name => "ゴルフ" , :ruby => "golf",  :matching => 0, :place => 0, :openchat => "", :txt => "ゴルフサークル" },
+  { :id => 29, :category_id => 3, :order => "0304", :name => "ボウリング" , :ruby => "bowling",  :matching => 0, :place => 0, :openchat => "", :txt => "ボウリングサークル" },
+  { :id => 30, :category_id => 3, :order => "0305", :name => "水泳" , :ruby => "swimming",  :matching => 0, :place => 0, :openchat => "", :txt => "水泳サークル・チーム" },
+  { :id => 31, :category_id => 3, :order => "0306", :name => "カバディ" , :ruby => "kabaddi",  :matching => 0, :place => 0, :openchat => "", :txt => "カバディサークル・チーム" },
+  { :id => 15, :category_id => 3, :order => "0307", :name => "オールラウンド" , :ruby => "all-round",  :matching => 2, :place => 0, :openchat => "", :txt => "オールラウンドサークル" },
 
-  { :id => 20, :category_id => 4, :order => "28", :name => "スノーボード" , :ruby => "snow-board",  :matching => 0, :place => 0, :openchat => "", :txt => "スノーボードサークル" },
+  # ウィンタースポーツ
+  { :id => 20, :category_id => 4, :order => "0401", :name => "スノーボード" , :ruby => "snow-board",  :matching => 0, :place => 0, :openchat => "", :txt => "スノーボードサークル" },
+  { :id => 32, :category_id => 4, :order => "0402", :name => "アイスホッケー" , :ruby => "ice-hockey",  :matching => 0, :place => 0, :openchat => "", :txt => "アイスホッケーサークル" },
 
-  { :id => 27, :category_id => 5, :order => "59", :name => "ヨガ" , :ruby => "yoga",  :matching => 0, :place => 0, :openchat => "", :txt => "ヨガサークル" },
-  { :id => 6, :category_id => 5, :order => "23", :name => "筋トレ・合トレ" , :ruby => "work-out",  :matching => 0, :place => 0, :openchat => "", :txt => "筋トレ・合トレ" },
+  # フィットネス
+  { :id => 27, :category_id => 5, :order => "0501", :name => "ヨガ" , :ruby => "yoga",  :matching => 0, :place => 0, :openchat => "", :txt => "ヨガサークル" },
+  { :id => 6, :category_id => 5, :order => "0502", :name => "筋トレ・合トレ" , :ruby => "work-out",  :matching => 0, :place => 0, :openchat => "", :txt => "筋トレ・合トレ" },
 
-  { :id => 1, :category_id => 6, :order => "51", :name => "写真・カメラ" , :ruby => "camera",  :matching => 0, :place => 0, :openchat => "https://line.me/ti/g2/MrGVZOgwkBEGzRpvagL-Cw", :txt => "写真・カメラサークル" },
-  { :id => 12, :category_id => 6, :order => "53", :name => "イベント" , :ruby => "event",  :matching => 0, :place => 0, :openchat => "", :txt => "イベントサークル" },
+  # 写真・カメラ
+  { :id => 1, :category_id => 6, :order => "0601", :name => "写真・カメラ" , :ruby => "camera",  :matching => 0, :place => 0, :openchat => "https://line.me/ti/g2/MrGVZOgwkBEGzRpvagL-Cw", :txt => "写真・カメラサークル" },
 
-  { :id => 17, :category_id => 7, :order => "26", :name => "ロードバイク" , :ruby => "road-bike",  :matching => 0, :place => 0, :openchat => "", :txt => "ロードバイクサークル" },
-  { :id => 18, :category_id => 7, :order => "52", :name => "登山" , :ruby => "mountain",  :matching => 0, :place => 0, :openchat => "", :txt => "登山サークル" },
+  # 趣味・交流
+  { :id => 33, :category_id => 7, :order => "0701", :name => "読書会" , :ruby => "reading",  :matching => 0, :place => 0, :openchat => "", :txt => "読書会・読書サークル" },
+  { :id => 34, :category_id => 7, :order => "0702", :name => "英会話" , :ruby => "english",  :matching => 0, :place => 0, :openchat => "", :txt => "英会話サークル" },
+  { :id => 35, :category_id => 7, :order => "0703", :name => "飲み会" , :ruby => "drinking",  :matching => 0, :place => 0, :openchat => "", :txt => "飲み会サークル" },
+  { :id => 36, :category_id => 7, :order => "0704", :name => "交流会" , :ruby => "gathering",  :matching => 0, :place => 0, :openchat => "", :txt => "交流会サークル" },
+  { :id => 37, :category_id => 7, :order => "0705", :name => "散歩" , :ruby => "walk",  :matching => 0, :place => 0, :openchat => "", :txt => "散歩サークル" },
+  { :id => 38, :category_id => 7, :order => "0706", :name => "イラスト" , :ruby => "illustration",  :matching => 0, :place => 0, :openchat => "", :txt => "イラストサークル" },
+  { :id => 12, :category_id => 7, :order => "0707", :name => "イベント" , :ruby => "event",  :matching => 0, :place => 0, :openchat => "", :txt => "イベントサークル" },
 
-  { :id => 19, :category_id => 8, :order => "24", :name => "ダンス" , :ruby => "dance",  :matching => 0, :place => 0, :openchat => "", :txt => "ダンスサークル" },
-  { :id => 14, :category_id => 8, :order => "25", :name => "よさこい" , :ruby => "yosakoi",  :matching => 0, :place => 0, :openchat => "", :txt => "よさこいチーム・サークル" },
+  # アウトドア
+  { :id => 18, :category_id => 8, :order => "0801", :name => "登山" , :ruby => "mountain",  :matching => 0, :place => 0, :openchat => "", :txt => "登山サークル" },
+  { :id => 39, :category_id => 8, :order => "0802", :name => "キャンプ" , :ruby => "camping",  :matching => 0, :place => 0, :openchat => "", :txt => "キャンプサークル" },
+  { :id => 17, :category_id => 8, :order => "0803", :name => "ロードバイク" , :ruby => "road-bike",  :matching => 0, :place => 0, :openchat => "", :txt => "ロードバイクサークル" },
 
-  { :id => 13, :category_id => 9, :order => "58", :name => "音楽" , :ruby => "music",  :matching => 0, :place => 0, :openchat => "", :txt => "音楽サークル" },
+  # ダンス・よさこい
+  { :id => 19, :category_id => 9, :order => "0901", :name => "ダンス" , :ruby => "dance",  :matching => 0, :place => 0, :openchat => "", :txt => "ダンスサークル" },
+  { :id => 14, :category_id => 9, :order => "0902", :name => "よさこい" , :ruby => "yosakoi",  :matching => 0, :place => 0, :openchat => "", :txt => "よさこいチーム・サークル" },
 
-  { :id => 11, :category_id => 10, :order => "57", :name => "ゲーム" , :ruby => "game",  :matching => 1, :place => 0, :openchat => "", :txt => "ボードゲーム・ゲームサークル" },
+  # 音楽・演劇
+  { :id => 13, :category_id => 10, :order => "1001", :name => "音楽" , :ruby => "music",  :matching => 0, :place => 0, :openchat => "", :txt => "音楽サークル" },
+  { :id => 40, :category_id => 10, :order => "1002", :name => "演劇" , :ruby => "theater",  :matching => 0, :place => 0, :openchat => "", :txt => "演劇サークル" },
 
-  { :id => 24, :category_id => 11, :order => "54", :name => "学生団体" , :ruby => "student-group",  :matching => 0, :place => 0, :openchat => "", :txt => "学生団体" },
-  { :id => 26, :category_id => 11, :order => "55", :name => "ボランティア" , :ruby => "volunteer",  :matching => 0, :place => 0, :openchat => "", :txt => "ボランティアサークル" }
+  # ゲーム
+  { :id => 11, :category_id => 11, :order => "1101", :name => "ボードゲーム" , :ruby => "game",  :matching => 1, :place => 0, :openchat => "", :txt => "ボードゲームサークル" },
+  { :id => 41, :category_id => 11, :order => "1102", :name => "ダーツ・ビリヤード" , :ruby => "darts",  :matching => 0, :place => 0, :openchat => "", :txt => "ダーツ・ビリヤードサークル" },
+  { :id => 42, :category_id => 11, :order => "1103", :name => "麻雀" , :ruby => "mahjong",  :matching => 0, :place => 0, :openchat => "", :txt => "麻雀サークル" },
 
+  # 学生団体・ボランティア
+  { :id => 24, :category_id => 12, :order => "1201", :name => "学生団体" , :ruby => "student-group",  :matching => 0, :place => 0, :openchat => "", :txt => "学生団体" },
+  { :id => 26, :category_id => 12, :order => "1202", :name => "ボランティア" , :ruby => "volunteer",  :matching => 0, :place => 0, :openchat => "", :txt => "ボランティアサークル" },
 
+  # その他
+  { :id => 99, :category_id => 99, :order => "9999", :name => "その他" , :ruby => "other",  :matching => 0, :place => 0, :openchat => "", :txt => "その他のサークル" }
 
 )
