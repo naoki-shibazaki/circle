e1fea1ad319d677deea6ff4025f5bc93472164b2
須沢友裕 <suzawa@MacBook-Air.local>
2021-04-04 16:57:47 +0900

    OGP description変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,28 +2,28 @@ module ApplicationHelper
 
   def default_meta_tags
       {
-        site: 'サークルブック',
-        title: 'サークルブック',
+        site: "サークルブック",
+        title: "サークルブック",
         reverse: true,
-        charset: 'utf-8',
-        description: '【完全無料／登録不要】47都道府県のサークルやチーム、団体のメンバー募集サイト！スポーツや趣味など豊富に掲載！10代〜60代、社会人や学生、初心者、経験者など、誰でも参加可能！バスケやバレー、フットサルなど。簡単に無料でメンバー募集ができます！',
-        keywords: 'サークル,チーム,団体,スポーツ,趣味,社会人,学生',
-        separator: '|',
+        charset: "utf-8",
+        description: "【完全無料／登録不要】47都道府県のサークルやチーム、団体のメンバー募集サイトです！スポーツや趣味など豊富に掲載！10代〜60代、社会人や学生、初心者、経験者など、誰でも参加可能！バスケやバレー、フットサルなど。無料で簡単にメンバー募集ができます！",
+        keywords: "サークル,チーム,団体,スポーツ,趣味,社会人,学生",
+        separator: "|",
         icon: [
-          { href: image_url('https://www.circle-book.com/images/favicon.ico')},
-          { href: image_url('https://www.circle-book.com/images/apple-touch-icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+          { href: image_url("https://www.circle-book.com/images/favicon.ico")},
+          { href: image_url("https://www.circle-book.com/images/apple-touch-icon.png"), rel: "apple-touch-icon", sizes: "180x180", type: "image/jpg" },
         ],
         og: {
           site_name: :site,
           title: :title,
           description: :description,
-          type: 'website',
+          type: "website",
           url: request.original_url,
-          image: image_url('/images/ogp.png'),
-          locale: 'ja_JP'
+          image: image_url("/images/ogp.png"),
+          locale: "ja_JP"
         },
         twitter: {
-            card: 'summary_large_image'
+            card: "summary_large_image"
         }
       }
   end
