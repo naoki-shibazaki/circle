b7f0f875ce7d7728dcd32f57bf6c710bf25f4f74
須沢友裕 <suzawa@suzawaiMac.local>
2021-04-23 01:05:24 +0900

    Place レビュー 途中

--- app/models/place_review.rb
+++ app/models/place_review.rb
@@ -1,3 +1,17 @@
 class PlaceReview < ApplicationRecord
   belongs_to :place
+
+	with_options presence: true do
+    validates :price
+    validates :facility
+    validates :access
+    validates :reservation
+    validates :comment
+	end
+
+	NGWORD = %w(http 死ね)
+	NGWORD_REGEX = %r(#{NGWORD.join('|')})
+	validates :comment, format: { without: NGWORD_REGEX }
+
+
 end
