507f54e8fa4f2a32d642e5f1c9e69cf0242f3fe8
須沢友裕 <suzawa@MacBook-Air.local>
2021-04-27 14:55:14 +0900

    DB ロールバック

--- db/migrate/20210427051244_change_column_to_place_reviews.rb
+++ db/migrate/20210427051244_change_column_to_place_reviews.rb
@@ -1,9 +1,5 @@
 class ChangeColumnToPlaceReviews < ActiveRecord::Migration[5.2]
   def change
-    remove_column :place_reviews, :facility, :float
-    remove_column :place_reviews, :reservation, :float
-    remove_column :place_reviews, :price, :float
-    remove_column :place_reviews, :access, :float
     remove_column :place_reviews, :facility, :string
     remove_column :place_reviews, :reservation, :string
     remove_column :place_reviews, :price, :string
