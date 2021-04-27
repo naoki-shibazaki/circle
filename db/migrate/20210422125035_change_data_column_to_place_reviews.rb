2f25b60fcd9846f8378d66d2aa5c823adfc47ef6
須沢友裕 <suzawa@suzawaiMac.local>
2021-04-27 11:36:14 +0900

    BD　ROLLBACK

--- db/migrate/20210422125035_change_data_column_to_place_reviews.rb
+++ db/migrate/20210422125035_change_data_column_to_place_reviews.rb
@@ -1,15 +1,3 @@
 class ChangeDataColumnToPlaceReviews < ActiveRecord::Migration[5.2]
-  def up
-    change_column :place_reviews, :facility, :float
-    change_column :place_reviews, :reservation, :float
-    change_column :place_reviews, :price, :float
-    change_column :place_reviews, :access, :float
-  end
 
-  def down
-    change_column :place_reviews, :facility, :string
-    change_column :place_reviews, :reservation, :string
-    change_column :place_reviews, :price, :string
-    change_column :place_reviews, :access, :string
-  end
 end
