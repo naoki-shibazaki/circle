43378caf16be3dea2423f37bbe76776472606249
須沢友裕 <suzawa@MacBook-Air.local>
2021-04-27 14:15:55 +0900

    BD ロールバック

---
+++ db/migrate/20210427051244_change_column_to_place_reviews.rb
@@ -0,0 +1,8 @@
+class ChangeColumnToPlaceReviews < ActiveRecord::Migration[5.2]
+  def change
+    change_column :place_reviews, :facility, :float
+    change_column :place_reviews, :reservation, :float
+    change_column :place_reviews, :price, :float
+    change_column :place_reviews, :access, :float
+  end
+end
