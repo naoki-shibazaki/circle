9d0d671ac0f9b212b99588938384a0d7ddcf4365
須沢友裕 <suzawa@MacBook-Air.local>
2021-04-27 15:04:41 +0900

    column再追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_051244) do
+ActiveRecord::Schema.define(version: 2021_04_27_060228) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -288,6 +288,10 @@ ActiveRecord::Schema.define(version: 2021_04_27_051244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "average_score"
+    t.float "facility"
+    t.float "reservation"
+    t.float "price"
+    t.float "access"
     t.index ["place_id"], name: "index_place_reviews_on_place_id"
   end
 
