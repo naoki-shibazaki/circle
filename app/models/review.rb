ac239216c6b7e1d810391e9675969674c7eac5aa
須沢友裕 <suzawa@MacBook-Air.local>
2021-05-10 17:57:53 +0900

    Place show ampエラー修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,14 +2,8 @@ class Review < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :member, optional: true
 
-
-	with_options presence: true do
-    validates :comment
-	end
-
 	NGWORD = %w(http 死ね)
 	NGWORD_REGEX = %r(#{NGWORD.join('|')})
 	validates :comment, format: { without: NGWORD_REGEX }
-
 
 end
