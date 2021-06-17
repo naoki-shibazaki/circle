2467eac8d6db367d68f97e2273945efc154da7d0
suzawa0420 <suzawa@photomo.jp>
2021-06-18 01:00:16 +0900

    Review NGワード追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :member, optional: true
 
-	NGWORD = %w(http 死ね)
+	NGWORD = %w(http 死ね bit й л com link)
 	NGWORD_REGEX = %r(#{NGWORD.join('|')})
-	validates :comment, format: { without: NGWORD_REGEX }
-
+  validates :comment, format: { without: NGWORD_REGEX, message: 'にNGワードが含まれています' }
 end
