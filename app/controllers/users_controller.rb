ba0b01fabd5bc155c642e87c516b38295618f620
suzawa0420 <suzawa@photomo.jp>
2021-12-28 14:45:47 +0900

    users_list 人気順追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -359,8 +359,8 @@ helper_method :link_count
     @ng_accounts = User.where(admin_user_id: @user.admin_user_id, ng_account: "NG")
 
     # サークルブックポイント計算
-    @blog_point = @user.blogs.count * 0.5
     @schedule_point = @user.schedules.count * 0.1
+    @blog_point = @user.blogs.count * 0.2
     @qa_point = Question.where(user_id: @user.id).where.not(answer: nil).count * 1
     @review_point = Review.where(user_id: @user.id, review: 1).count * 3
 
