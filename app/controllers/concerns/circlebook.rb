eacdf46bd6e5c4d621bd631842ae7108c62f230b
suzawa0420 <suzawa@photomo.jp>
2021-12-28 14:26:08 +0900

    マイページ　サークルブックポイント　表示

--- app/controllers/concerns/circlebook.rb
+++ app/controllers/concerns/circlebook.rb
@@ -7,8 +7,8 @@ module Circlebook
         # 違反者
         user.cb_point = -100
       else
-        blog_point = user.blogs.count * 0.5
         schedule_point = user.schedules.count * 0.1
+        blog_point = user.blogs.count * 0.2
         qa_point = Question.where(user_id: user.id).where.not(answer: nil).count * 1
         review_point = Review.where(user_id: user.id, review: 1).count * 3
         user.cb_point = blog_point + schedule_point + qa_point + review_point
