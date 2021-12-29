ffc92e4a06ba4a86bfd54301f0adc208068e8796
suzawa0420 <suzawa@photomo.jp>
2021-12-30 01:35:35 +0900

    更新1年以上なし　CP 0

--- app/controllers/concerns/circlebook.rb
+++ app/controllers/concerns/circlebook.rb
@@ -3,18 +3,22 @@ module Circlebook
 
     # サークルブックポイント
     def cb_point(user)
+      @schedule_point = user.schedules.count * 0.1
+      @blog_point = user.blogs.count * 0.2
+      @qa_point = Question.where(user_id: user.id).where.not(answer: nil).count * 0.5
+      @review_point = Review.where(user_id: user.id, review: 1).count * 2
+      @contact_point = UserContact.where(user_id: user.id, contact_del: nil).count * 0.5
+      @respond_point = UserContact.where(user_id: user.id, respond_check: "NG").count * 10
+
       if user.admin_user.check.present? && user.admin_user.check != 0
         # 違反者
         user.cb_point = -100
+      elsif user.user_time.to_time <= Time.zone.now.ago(1.years)
+        user.cb_point = 0
       else
-        @schedule_point = user.schedules.count * 0.1
-        @blog_point = user.blogs.count * 0.2
-        @qa_point = Question.where(user_id: user.id).where.not(answer: nil).count * 0.5
-        @review_point = Review.where(user_id: user.id, review: 1).count * 2
-        @contact_point = UserContact.where(user_id: user.id, contact_del: nil).count * 0.5
-        @respond_point = UserContact.where(user_id: user.id, respond_check: "NG").count * 10
         user.cb_point = @blog_point + @schedule_point + @qa_point + @review_point + @contact_point - @respond_point
       end
+
     end
 
     # ソート用の日付更新
