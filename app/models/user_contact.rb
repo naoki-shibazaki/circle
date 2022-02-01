5aa36f3412b3e7e6f5531ef3be8a1605bca75dc1
suzawa0420 <suzawa@photomo.jp>
2022-02-01 12:24:23 +0900

    NGワード追加

--- app/models/user_contact.rb
+++ app/models/user_contact.rb
@@ -6,7 +6,7 @@ class UserContact < ApplicationRecord
             format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角の日本語（漢字／ひらがな／カタカナ）で入力してください' }
 
   validates :message, presence: true, length: { maximum: 500, message: 'は500文字以内でお願いします' }
-	NGWORD = %w(http 株式会社 弊社 副業 ダイニング 先行予約 割引 ヒアリング調査 アンケート調査 ご来場 プレゼント アースフレンズ)
+	NGWORD = %w(http 株式会社 弊社 副業 ダイニング 先行予約 割引 ヒアリング調査 アンケート調査 ご来場 プレゼント アースフレンズ ロッツ横浜)
 	NGWORD_REGEX = %r(#{NGWORD.join('|')})
 	validates :message, format: { without: NGWORD_REGEX, message: 'にNGワードが含まれています' }
 
