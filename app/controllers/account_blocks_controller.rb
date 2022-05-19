7275a95ae39564666178896897e64d9d85d62a01
suzawa0420 <suzawa@photomo.jp>
2022-05-19 16:13:47 +0900

    質問コーナー修正

--- app/controllers/account_blocks_controller.rb
+++ app/controllers/account_blocks_controller.rb
@@ -1,5 +1,6 @@
 class AccountBlocksController < ApplicationController
-  before_action :webmaster
+
+  before_action :webmaster, {only: [:index, :destroy]}
 
   def index
     @account_blocks = AccountBlock.all.order(created_at: "DESC")
@@ -17,15 +18,21 @@ class AccountBlocksController < ApplicationController
     @user = User.find_by(id: params[:user_id])
     @question = Question.find(params[:id])
 
-    @account_block = AccountBlock.new
-    @account_block.block = 1
-    @account_block.ip_address = @question.ip_address
-    @account_block.model = "Question"
-    @account_block.url = request.url
-    @account_block.save
+    if current_admin_user.id == @user.admin_user_id || current_admin_user.id == 1
+      @account_block = AccountBlock.new
+      @account_block.block = 1
+      @account_block.ip_address = @question.ip_address
+      @account_block.model = "Question"
+      @account_block.url = request.url
+      @account_block.save
 
-		flash[:notice] = "通報しました"
-		redirect_to user_questions_path
+      flash[:notice] = "通報しました"
+      redirect_to user_questions_path
+
+    else
+      flash[:notice] = "権限がありません"
+      redirect_to users_path
+    end
 
   end
 
