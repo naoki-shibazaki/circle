dbd7e144ad956eed75a108ee0e098ff2c7587216
suzawa0420 <suzawa@photomo.jp>
2023-03-18 00:44:23 +0900

    no message

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -283,60 +283,59 @@ def day
   @b4_url = ""
 end
 
+private
+  def set_schedules
+    @user = User.find(params[:user_id])
+    gallery_counts(@user)
+    @schedules = Schedule.where(user_id: @user.id).where("day > ?", DateTime.yesterday).order(:day => :asc, :time_s => :asc)
+    @past_schedules = Schedule.where(user_id: @user.id).where("day <= ?", DateTime.yesterday).order(:day => :desc, :time_s => :asc).page(params[:page])
+    @data = AdminUser.find_by(id: params[:user_id])
+    @prefectures = Prefecture.all
+    @schedule_month = 0
+    @schedule_m_01 = 0
+    @schedule_m_02 = 0
+    @wdays =  ["日", "月", "火", "水", "木", "金", "土" ]
 
-	private
-		def set_schedules
-			@user = User.find(params[:user_id])
-      gallery_counts(@user)
-			@schedules = Schedule.where(user_id: @user.id).where("day > ?", DateTime.yesterday).order(:day => :asc, :time_s => :asc)
-			@past_schedules = Schedule.where(user_id: @user.id).where("day <= ?", DateTime.yesterday).order(:day => :desc, :time_s => :asc).page(params[:page])
-			@data = AdminUser.find_by(id: params[:user_id])
-			@prefectures = Prefecture.all
-			@schedule_month = 0
-			@schedule_m_01 = 0
-			@schedule_m_02 = 0
-      @wdays =  ["日", "月", "火", "水", "木", "金", "土" ]
+    @mail_title = "【#{@user.name}】お問い合わせ"
+    @mail_message = "こちらに相談内容をご記入ください！"
+  end
 
-			@mail_title = "【#{@user.name}】お問い合わせ"
-			@mail_message = "こちらに相談内容をご記入ください！"
-		end
+  def set_attendances
+    @user = User.find_by(unique_id: params[:unique_id])
+    gallery_counts(@user)
+    @schedules = Schedule.where(user_id: @user.id).where("day > ?", DateTime.yesterday).order(:day => :asc, :time_s => :asc)
+    @past_schedules = Schedule.where(user_id: @user.id).where("day <= ?", DateTime.yesterday).order(:day => :desc, :time_s => :asc)
+  end
 
-    def set_attendances
-      @user = User.find_by(unique_id: params[:unique_id])
-      gallery_counts(@user)
-			@schedules = Schedule.where(user_id: @user.id).where("day > ?", DateTime.yesterday).order(:day => :asc, :time_s => :asc)
-			@past_schedules = Schedule.where(user_id: @user.id).where("day <= ?", DateTime.yesterday).order(:day => :desc, :time_s => :asc)
+  def set_dates
+    @wdays =  ["日", "月", "火", "水", "木", "金", "土" ]
+    if admin_user_signed_in?
+      @user = current_admin_user.users.last
     end
+  end
 
-    def set_dates
-      @wdays =  ["日", "月", "火", "水", "木", "金", "土" ]
-      if admin_user_signed_in?
-        @user = current_admin_user.users.last
-      end
+
+  def ensure_correct_user
+    @user = User.find(params[:user_id])
+
+    if current_admin_user.id.to_i == @user.admin_user_id.to_i
+
+    elsif current_admin_user.id == 1
+
+    else
+      flash[:notice] = "権限がありません"
+      redirect_to users_path
+
     end
+  end
 
+  def schedule_params
+    params.require(:schedule).permit(:day, :venue, :date, :time_s, :time_e, :venue_address, :note, :title, :google_map, :recruitment, :member_venue, :recruitment_numbers, :cost, :top_image)
+  end
 
-		def ensure_correct_user
-			@user = User.find(params[:user_id])
-
-      if current_admin_user.id.to_i == @user.admin_user_id.to_i
-
-      elsif current_admin_user.id == 1
-
-			else
-        flash[:notice] = "権限がありません"
-        redirect_to users_path
-
-			end
-		end
-
-		def schedule_params
-			params.require(:schedule).permit(:day, :venue, :date, :time_s, :time_e, :venue_address, :note, :title, :google_map, :recruitment, :member_venue, :recruitment_numbers, :cost, :top_image)
-		end
-
-    def name_params
-			params.require(:name).permit(:name, :gender, schedule_ids:[], name_schedules_attributes: [:answer, :comment, :schedule_id, :name_id, :_destroy, :id])
-		end
+  def name_params
+    params.require(:name).permit(:name, :gender, schedule_ids:[], name_schedules_attributes: [:answer, :comment, :schedule_id, :name_id, :_destroy, :id])
+  end
 
 
 end
