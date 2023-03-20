7734c3e2815064d571074450d1995e19b7ebb078
suzawa0420 <suzawa@photomo.jp>
2023-03-20 15:56:14 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   credentials = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
   Aws::Rails.add_action_mailer_delivery_method(
     :ses,
-    credentials:,
+    credentials: creds,
     region: ENV['AWS_S3_REGION']
   )
 
