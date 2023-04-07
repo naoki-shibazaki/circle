d7a7198cdeb9b365a6ef59f8fe5dd4f7616f66c7
suzawa0420 <suzawa@photomo.jp>
2023-04-07 13:37:27 +0900

    cloudfront コメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.action_controller.asset_host = 'd2vaopj9xwgout.cloudfront.net'
+  # config.action_controller.asset_host = 'd2vaopj9xwgout.cloudfront.net'
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
