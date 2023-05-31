8c750be78b55522405517fccb093cff61170b1db
suzawa0420 <suzawa@photomo.jp>
2023-05-31 15:18:04 +0900

    修正

--- app/controllers/exhibitor_apps/exhibitor_mypages_controller.rb
+++ app/controllers/exhibitor_apps/exhibitor_mypages_controller.rb
@@ -1,6 +1,9 @@
 class ExhibitorApps::ExhibitorMypagesController < ExhibitorApps::ApplicationController
 
   def show
+    if current_exhibition_group.profile.blank?
+      redirect_to edit_exhibitor_profile_path, notice: 'プロフィールを登録してください'
+    end
   end
 
 
