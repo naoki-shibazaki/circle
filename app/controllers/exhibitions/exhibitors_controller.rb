dd8bdeb18567502a5487cee1b53b5580964c6504
suzawa0420 <suzawa@photomo.jp>
2023-09-01 22:47:44 +0900

    写真展ページの作成

--- app/controllers/exhibitions/exhibitors_controller.rb
+++ app/controllers/exhibitions/exhibitors_controller.rb
@@ -2,6 +2,7 @@ class Exhibitions::ExhibitorsController < Exhibitions::ApplicationController
 
   def show
     @exhibitor = ExhibitionGroupProfile.find(params[:id])
+    @exhibitions = Exhibition.where(exhibition_group_id: @exhibitor.id).includes([:prefecture, :exhibition_group]).order(end_date: "desc")
   end
 
 end
