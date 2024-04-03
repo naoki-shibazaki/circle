f03421c0effaa9a93d04b7bc38830a7269eb5a51
suzawa0420 <suzawa@photomo.jp>
2024-04-03 17:25:12 +0900

    出欠登録の不具合修正、ブログの本文必須、サクブポイント見直し

--- app/controllers/concerns/circlebook.rb
+++ app/controllers/concerns/circlebook.rb
@@ -1,13 +1,13 @@
 module Circlebook
   extend ActiveSupport::Concern
 
-    # サークルブックポイント
+    # サークルブックポイント（サクブポイント）
     def cb_point(user)
       @schedule_point = user.schedules.count * 0.1
       @blog_point = user.blogs.count * 0.2
       @qa_point = Question.where(user_id: user.id).where.not(answer: nil).count * 0.3
       @review_point = Review.where(user_id: user.id, review: 1).count * 2
-      @contact_point = UserContact.where(user_id: user.id, contact_del: nil).count * 0.5
+      # @contact_point = UserContact.where(user_id: user.id, contact_del: nil).count * 0.5
       @respond_point = UserContact.where(user_id: user.id, respond_check: "NG").count * 30
 
       if user.admin_user.check.present? && user.admin_user.check != 0
@@ -18,7 +18,8 @@ module Circlebook
       elsif user.user_time.to_time <= Time.zone.now.ago(1.years)
         user.cb_point = 0
       else
-        user.cb_point = @blog_point + @schedule_point + @qa_point + @review_point + @contact_point - @respond_point
+        # user.cb_point = @blog_point + @schedule_point + @qa_point + @review_point + @contact_point - @respond_point
+        user.cb_point = @blog_point + @schedule_point + @qa_point + @review_point - @respond_point
       end
 
     end
